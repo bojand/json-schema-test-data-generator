408c61377fdc02dfe4a5d545c7c13689cdba1034
Bojan Djurkovic <dbojan@gmail.com>
2016-04-01 20:58:31 -0300

    negative types

--- test/index.js
+++ test/index.js
@@ -15,7 +15,7 @@ test('should generate test data with differnte variations based on required prop
     return v === true && diff.length === 0;
   });
 
-  t.is(found, true);
+  t.true(found);
 
   required.forEach(req => {
     // check that there is sample data that does not have the required property
@@ -24,7 +24,27 @@ test('should generate test data with differnte variations based on required prop
       return d.valid === false && keys.indexOf(req) === -1;
     });
 
-    t.is(found, true);
+    t.true(found, `test failed for property: ${req}`);
+  });
+
+  t.pass();
+});
+
+test('should generate test data with negative type tests for simple primitive data types', t => {
+  const required = userSchema.required;
+  const data = generate(userSchema);
+
+  required.forEach(req => {
+    const found = _.chain(data)
+      .filter(d => (d.valid === false) && (d.property && d.property === req) && (typeof d.data[req] !== 'undefined'))
+      .some(d => {
+        const types = generate.getNegativeTypes(userSchema.properties[req]);
+        const dataType = d.data[req] === null ? 'null' : typeof d.data[req];
+        const check = types.indexOf(dataType) >= 0;
+        return check;
+      }).value();
+
+    t.true(found, `test failed for property: ${req}`);
   });
 
   t.pass();
