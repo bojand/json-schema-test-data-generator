056484966dd1bc86fe7260ab7eb49df23dea0d09
Bojan Djurkovic <dbojan@gmail.com>
2016-04-02 10:41:33 -0300

    fixes and more tests

--- test/index.js
+++ test/index.js
@@ -1,6 +1,8 @@
 import test from 'ava';
 import generate from '../dist/index';
 import _ from 'lodash';
+const validator = require('validator');
+
 const userSchema = require('./user.json');
 
 test('should generate test data with differnte variations based on required property', t => {
@@ -75,6 +77,235 @@ test('should create negative for number multipleOf', t => {
 
     t.true(found, `test failed for multipleOf of property: ${req}`);
   });
+
+  t.pass();
+});
+
+test('should create negative for number maximum', t => {
+  const schema = {
+    type: 'object',
+    properties: {
+      foo: {
+        type: 'number',
+        maximum: 3
+      }
+    }
+  };
+
+  const data = generate(schema);
+  const found = _.chain(data)
+    .filter(d => d.valid === false && typeof d.data.foo === 'number')
+    .some(d => d.data.foo && d.data.foo > 3)
+    .value();
+
+  t.true(found, `test failed for maximum`);
+
+  t.pass();
+});
+
+test('should create negative for number minimum', t => {
+  const schema = {
+    type: 'object',
+    properties: {
+      foo: {
+        type: 'number',
+        minimum: 3
+      }
+    }
+  };
+
+  const data = generate(schema);
+  const found = _.chain(data)
+    .filter(d => d.valid === false && typeof d.data.foo === 'number')
+    .some(d => d.data.foo && d.data.foo < 3)
+    .value();
+
+  t.true(found, `test failed for minimum`);
+
+  t.pass();
+});
+
+test('should create negative for string maxLength', t => {
+  const schema = {
+    type: 'object',
+    properties: {
+      foo: {
+        type: 'string',
+        maxLength: 5
+      }
+    }
+  };
+
+  const data = generate(schema);
+  const found = _.chain(data)
+    .filter(d => d.valid === false && typeof d.data.foo === 'string')
+    .some(d => d.data.foo && d.data.foo.length > 5)
+    .value();
+
+  t.true(found, `test failed for maxLength`);
+
+  t.pass();
+});
+
+test('should create negative for string minLength', t => {
+  const schema = {
+    type: 'object',
+    properties: {
+      foo: {
+        type: 'string',
+        minLength: 5
+      }
+    }
+  };
+
+  const data = generate(schema);
+  const found = _.chain(data)
+    .filter(d => d.valid === false && typeof d.data.foo === 'string')
+    .some(d => d.data.foo && d.data.foo.length < 5)
+    .value();
+
+  t.true(found, `test failed for minLength`);
+
+  t.pass();
+});
+
+test('should create negative for string pattern date-time', t => {
+  const schema = {
+    type: 'object',
+    properties: {
+      foo: {
+        type: 'string',
+        format: 'date-time'
+      }
+    }
+  };
+
+  const data = generate(schema);
+  const found = _.chain(data)
+    .filter(d => d.valid === false && typeof d.data.foo === 'string')
+    .some(d => d.data.foo && isNaN(Date.parse(d.data.foo)))
+    .value();
+
+  t.true(found, `test failed for validation of pattern date-time`);
+
+  t.pass();
+});
+
+test('should create negative for string pattern email', t => {
+  const schema = {
+    type: 'object',
+    properties: {
+      foo: {
+        type: 'string',
+        format: 'email'
+      }
+    }
+  };
+
+  const data = generate(schema);
+  const found = _.chain(data)
+    .filter(d => d.valid === false && typeof d.data.foo === 'string')
+    .some(d => d.data.foo && !validator.isEmail(d.data.foo))
+    .value();
+
+  t.true(found, `test failed for validation of pattern email`);
+
+  t.pass();
+});
+
+test('should create negative for string pattern uri', t => {
+  const schema = {
+    type: 'object',
+    properties: {
+      foo: {
+        type: 'string',
+        format: 'uri'
+      }
+    }
+  };
+
+  const data = generate(schema);
+  const found = _.chain(data)
+    .filter(d => d.valid === false && typeof d.data.foo === 'string')
+    .some(d => d.data.foo && !validator.isURL(d.data.foo))
+    .value();
+
+  t.true(found, `test failed for validation of pattern uri`);
+
+  t.pass();
+});
+
+test('should create negative for array property maxItems', t => {
+  const schema = {
+    type: 'object',
+    properties: {
+      foo: {
+        type: 'array',
+        items: {
+          type: 'string'
+        },
+        maxItems: 5
+      }
+    }
+  };
+
+  const data = generate(schema);
+  const found = _.chain(data)
+    .filter(d => d.valid === false && Array.isArray(d.data.foo))
+    .some(d => d.data.foo && d.data.foo.length > 5)
+    .value();
+
+  t.true(found, `test failed for validation of maxItems`);
+
+  t.pass();
+});
+
+test('should create negative for array property minItems', t => {
+  const schema = {
+    type: 'object',
+    properties: {
+      foo: {
+        type: 'array',
+        items: {
+          type: 'string'
+        },
+        minItems: 5
+      }
+    }
+  };
+
+  const data = generate(schema);
+  const found = _.chain(data)
+    .filter(d => d.valid === false && Array.isArray(d.data.foo))
+    .some(d => d.data.foo && d.data.foo.length < 5)
+    .value();
+
+  t.true(found, `test failed for validation of minItems`);
+
+  t.pass();
+});
+
+test('should create negative for array property uniqueItems', t => {
+  const schema = {
+    type: 'object',
+    properties: {
+      foo: {
+        type: 'array',
+        items: {
+          type: 'string'
+        },
+        uniqueItems: true
+      }
+    }
+  };
+
+  const data = generate(schema);
+  const found = _.chain(data)
+    .filter(d => d.valid === false && Array.isArray(d.data.foo))
+    .some(d => d.data.foo && _.uniq(d.data.foo).length < d.data.foo.length)
+    .value();
+
+  t.true(found, `test failed for validation of uniqueItems`);
 
   t.pass();
 });
